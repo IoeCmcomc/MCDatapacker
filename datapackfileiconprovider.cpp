908170206bb01953977d4eb0f4692a166a09ee30
IoeCmcomc <53734763+IoeCmcomc@users.noreply.github.com>
2020-12-05 20:56:56 +0700

    [Minor] Add custom icons to the tree view
    CodeFile:
    - Add "Meta" value into FileType enum

---
+++ datapackfileiconprovider.cpp
@@ -0,0 +1,6 @@
+#include "datapackfileiconprovider.h"
+
+DatapackFileIconProvider::DatapackFileIconProvider()
+{
+
+}
---
+++ datapackfileiconprovider.h
@@ -0,0 +1,11 @@
+#ifndef DATAPACKFILEICONPROVIDER_H
+#define DATAPACKFILEICONPROVIDER_H
+
+
+class DatapackFileIconProvider : public QFileIconProvider
+{
+public:
+    DatapackFileIconProvider();
+};
+
+#endif // DATAPACKFILEICONPROVIDER_H
