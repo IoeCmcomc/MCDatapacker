b5d11391cf6d21ef97394d5ead1147691000480c
IoeCmcomc <53734763+IoeCmcomc@users.noreply.github.com>
2024-08-06 10:06:14 +0700

    [Impl} TagSelectorDialog now accepts unknown tags

--- src/tagselectordialog.cpp
+++ src/tagselectordialog.cpp
@@ -8,6 +8,8 @@
 #include <QDebug>
 #include <QDir>
 #include <QFileInfo>
+#include <QPushButton>
+
 
 TagSelectorDialog::TagSelectorDialog(QWidget *parent,
                                      CodeFile::FileType type) :
@@ -18,12 +20,12 @@ TagSelectorDialog::TagSelectorDialog(QWidget *parent,
     model.setParent(this);
     setupTagView(type);
     connect(ui->tagFilterBox, &QLineEdit::textChanged,
-            this, [this](const QString &input) {
-        filterModel.setFilterRegularExpression(input);
-    });
+            this, &TagSelectorDialog::onFilterChanged);
 
     connect(ui->tagListView->selectionModel(),
             &QItemSelectionModel::selectionChanged,
+            this, &TagSelectorDialog::checkOK);
+    connect(ui->selectAnywayCheck, &QCheckBox::stateChanged,
             this, &TagSelectorDialog::checkOK);
 
     connect(ui->tagListView->selectionModel(),
@@ -116,11 +118,15 @@ QString TagSelectorDialog::getInternalSelectedID() {
 }
 
 QString TagSelectorDialog::getSelectedID() {
-    return '#' + getInternalSelectedID();
+    const QString &&id = (ui->selectAnywayCheck->isChecked())
+                             ? ui->tagFilterBox->text() : getInternalSelectedID();
+
+    return '#' + id;
 }
 
 void TagSelectorDialog::checkOK() {
-    selectButton->setEnabled(!getInternalSelectedID().isEmpty());
+    selectButton->setEnabled(!getInternalSelectedID().isEmpty()
+                             || ui->selectAnywayCheck->isChecked());
 }
 
 void TagSelectorDialog::showDetails() {
@@ -135,4 +141,14 @@ void TagSelectorDialog::showDetails() {
         }
     }
     ui->tagDetailsLabel->setText(QString());
+}
+
+void TagSelectorDialog::onFilterChanged(const QString &text) {
+    filterModel.setFilterRegularExpression(text);
+    if (!text.isEmpty() && (ui->tagListView->model()->rowCount() == 0)) {
+        ui->stackedWidget->setCurrentIndex(1);
+    } else {
+        ui->stackedWidget->setCurrentIndex(0);
+        ui->selectAnywayCheck->setChecked(false);
+    }
 }
